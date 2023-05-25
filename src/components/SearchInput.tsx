230cbfc93a3822ad433f1b7e54fea40c239b6ead
Mahfuj Ahmed <ahmed.mahfuj02@gmail.com>
2023-05-26 02:19:01 +0600

    Fix the navber

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,24 +1,35 @@
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import React, { useRef } from 'react'
-import { BsSearch } from 'react-icons/bs'
-import useGameQueryStore from '../store'
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React, { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
-    const setSearchText = useGameQueryStore(s => s.setSearchText)
-    const ref = useRef<HTMLInputElement>(null)
-
-    return (
-    <form onSubmit={(event) =>{
+  return (
+    <form
+      onSubmit={(event) => {
         event.preventDefault();
-        if(ref.current)setSearchText(ref.current.value)
-    }}>
-    <InputGroup>
-    <InputLeftElement children={<BsSearch />} />
-    <Input ref={ref} borderRadius={20} placeholder='Search games...' variant='filled' />
-    </InputGroup>
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
     </form>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
